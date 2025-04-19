526c29e27f35e5ef128ea0cc36016516421a86b9
sravanireddy-06 <sravani.v.reddy4@gmail.com>
2025-04-19 10:09:45 +0530

    fix: port

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingsRouter);
-const PORT = process.env.PORT || 800;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
